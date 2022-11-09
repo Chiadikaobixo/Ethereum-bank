df92e1a29a18297a3612f343594fa566008a87e2
chiadikaobixo <chiadikaobixo@gmail.com>
2022-11-09 08:08:48 -0600

    migrated to Goerli Etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
-const INFURA_URL= process.env.INFURA_URL;
+const GOERLI_URL= process.env.GOERLI_URL;
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    rinkeby: {
-      url: INFURA_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
